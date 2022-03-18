35a1056dbf3065304ebc3022fae8678f54b08111
Naam Pondpat <naam.pt@gmail.com>
2022-03-18 09:26:43 -0400

    created TextBlock_MouseDown method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -53,15 +53,61 @@ namespace MatchGame
 
                 animalEmoji.RemoveAt(index);
             }
-           
+        }
 
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
 
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
 
+            // condition for the first click 
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+                return;
+            }
+
+            // condition for the second click - check if matched
+            var itemMatched = textBlock.Text == lastTextBlockClicked.Text;
+            if (itemMatched)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            } else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
         }
 
 
 
-        
-        
+        // handle click
+
+        // step 1: the user click on one of the textblock.
+
+        // step 2: first animal will appears then disappear. firstIndex
+
+        // step 3: the user click on the second textblock.
+
+        // step 4: second animal appears. secondIndex
+        /* check if match
+         * if (firstIndex == secondIndex) 
+         * {
+         *   firstIndex will reappear
+         *   
+         * } else {
+         *   firstIndex && secondIndex will disappear
+         * 
+         * }
+         * 
+         * 
+         * 
+         */
+
     }
 }
